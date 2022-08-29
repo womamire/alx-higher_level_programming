ee3691b42064bb325ac3f0ebc3f4ecf990b7dc99
womamire <mihretutd@gmail.com>
2022-08-29 21:39:27 +0300

    Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+
     from calculator_1 import add, sub, mul, div
-    if len(argv) != 4:
-        print("Usage:", argv[0], "<a> <operator> <b>")
-        exit(1)
-    a, b = int(argv[1]), int(argv[3])
-    operator = argv[2]
-    math = {"+": add, "-": sub, "*": mul, "/": div}
-    if operator not in math:
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    blt_op = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(blt_op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    print("{:d} {} {:d} = {:d}".format(a, operator, b, math[operator](a, b)))
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    print("{} {} {} = {}".format(a, sys.argv[2], b, blt_op[sys.argv[2]](a, b)))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "__":
-            print("{:s}".format(s))
+    built = dir(hidden_4)
+    for name in built:
+        if name[:2] != "__":
+            print(name)
